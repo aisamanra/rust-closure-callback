970f140e60df2ac6fbf5d5993bc9a4c30dc4b0cc
Getty Ritter <gdritter@galois.com>
2017-11-28 10:59:17 -0800

    Update callback example with self pointers

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
+// In this case, I'm using 'mut' in a few places it's not strictly
+// necessary, as a reminder. In particular: when we pass the 'self'
+// object in to our `callback` function, we're doing so because the
+// callback will eventually have mutable access to it. But in the
+// `callback` function itself, we don't use it mutably: we cast it to
+// an opaque C pointer, which means from Rust's point of view, we
+// don't _actually_ mutate it. But we're going to cast it back to a
+// mutable pointer eventually, which means passing a pointer in to
+// `callback` might involve it being mutated, even in Rust doesn't
+// realize it!
+#![allow(unused_mut)]
+
 #![feature(libc)]
 extern crate libc;
 
 use libc::*;
 use std::mem;
+
+// Here is a very silly struct with some internal state
+#[derive(Debug)]
+struct Thingy {
+    count: i32,
+}
+
+// And a few methods on it that visibly update that state
+impl Thingy {
+    fn add_count(&mut self, n: i32) {
+        self.count += n;
+        println!("updated count to {}", self.count);
+    }
+
+    fn decrement_count(&mut self, _: i32) {
+        self.count -= 1;
+        println!("decremented count to {}", self.count);
+    }
+}
 
 extern {
     // Our external C function here takes two arguments: one is a
@@ -17,35 +48,41 @@ extern {
 // pointer...
 extern "C" fn apply_closure(n: i32, data: *const c_void) {
     // and here it interprets the void pointer as a reference to a
-    // pointer to a function. The two layers are doing something
-    // important: pointers to traits in Rust are "fat pointers",
-    // i.e. they are actually a function-pointer/data-pointer pair. So
-    // what we're doing is getting a pointer to those two pointer
-    // pairs, and then using those to call the code with the
-    // appropriate data.
-    let closure: &mut Box<Fn(i32) -> ()> = unsafe { mem::transmute(data) };
-    // And then we can just call the closure!
-    closure(n);
+    // pair of pointers, one to a function and one to the 'self'
+    // object we're concerned with.
+    let mut payload: &mut (Box<Fn(&mut Thingy, i32) -> ()>, &mut Thingy) =
+        unsafe { mem::transmute(data) };
+
+    // Then, we call the closure with the 'self' object as first
+    // parameter. If the function is a method of Thingy, then this
+    // will work identically.
+    payload.0(&mut payload.1, n);
 }
 
-// This is a nicer wrapper function to providing the closure/callback
-// to C:
-fn callback<F>(cb: F)
-    where F: Fn(i32)
-{
-    // For the reasons articulated above, we create a pointer to a
-    // (fat) pointer to the closure
-    let boxed_cb: Box<Box<Fn(i32)>> = Box::new(Box::new(cb));
-    // and then we call the C function with our 'apply_closure'
-    // function as well as the actual closure:
-    unsafe {
-        call_callback(apply_closure,
-                      Box::into_raw(boxed_cb) as *const c_void);
+impl Thingy {
+    // This is a nicer wrapper function to providing the closure/callback
+    // to C:
+    fn callback<F>(&mut self, cb: F)
+        where F: Fn(&mut Thingy, i32)
+    {
+        // For the reasons articulated above, we create a pointer to a
+        // pair of the boxed function _and_ the pointer to Thingy
+        let boxed_cb: Box<(Box<Fn(&mut Thingy, i32)>, &mut Thingy)> =
+            Box::new((Box::new(cb), self));
+        // and then we call the C function with our 'apply_closure'
+        // function as well as the actual closure/self-pointer pair,
+        // passed as a void pointer
+        unsafe {
+            call_callback(apply_closure,
+                          Box::into_raw(boxed_cb) as *const c_void);
+        }
     }
 }
 
 // And now we can use it!
 fn main() {
-    let x = 4;
-    callback(|n| println!("Got: {}", n + x));
+    let mut t = Thingy { count: 0 };
+    t.callback(Thingy::add_count);
+    t.callback(Thingy::decrement_count);
+    println!("Final status: {:?}", t);
 }
